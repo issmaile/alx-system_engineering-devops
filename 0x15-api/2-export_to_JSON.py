533d36d627868082556d4dfbfe82884b00da1112
issmaile <ismail25smello@gmail.com>
2024-01-02 23:42:24 +0100

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" module documentation """
+import requests
+import json
+import sys
+
+
+def main():
+    """ main docum """
+    id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/'
+    users = f'users?id={id}'
+    todos = f'todos?userId={id}'
+    done = f'{todos}&completed=true'
+    notDone = f'{todos}&completed=false'
+    userData = requests.get(f'{url}{users}').json()
+    Name = userData[0].get("name")
+    userName = userData[0].get("username")
+    todosData = requests.get(f'{url}{todos}').json()
+    todosDone = requests.get(f'{url}{done}').json()
+    doneN = len(todosDone)
+    totalN = len(todosData)
+    with open(f'{id}.json', 'w') as file:
+        content = {
+                id: [
+                    {
+                        "task": task.get("title"),
+                        "completed": task.get("completed"),
+                        "username": userName,
+                    }
+                    for task in todosData
+                    ]
+                }
+        json.dump(data, file)
+
+
+if __name__ == "__main__":
+    main()
