5ac963c847936fc450ac716cf48ed725300a330b
issmaile <ismail25smello@gmail.com>
2024-09-27 02:45:21 +0100

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+"""Module for querying the Reddit API for subreddit information."""
 import requests
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    
+
+    # Make API request and prevent redirects
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
+    # Return number of subscribers if response is successful
     if response.status_code == 200:
         data = response.json()
         return data.get('data', {}).get('subscribers', 0)
-    else:
-        return 0
+    return 0
 
